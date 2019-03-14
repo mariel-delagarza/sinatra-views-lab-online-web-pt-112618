f5ecec41e3aff800ab39d351644a4eea8c6cca6e
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-03-14 01:58:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,16 +4,16 @@ class App < Sinatra::Base
 		erb :index
 	end
 
-  get '/hello' do 
-    erb :hello 
-  end 
+  get '/hello' do
+    erb :hello
+  end
 
-  get '/goodbye' do 
-    erb :goodbye 
-  end 
+  get '/goodbye' do
+    erb :goodbye
+  end
 
-  get '/date' do 
-    erb :date 
-  end 
+  get '/date' do
+    erb :date
+  end
 
 end
